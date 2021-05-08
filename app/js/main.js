10448cdefa4f2a8409e78f62052d1515339bc35f
IonShive <vlad.balaban2020@gmail.com>
2021-05-08 16:37:33 +0300

    added media and fix

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,17 @@
 $(function () {
 
+	$(".header__list-link, .header__link, .header__link-bottom").on("click", function (event) {
+		event.preventDefault();
+		var id = $(this).attr('href'),
+			top = $(id).offset().top;
+		$('body,html').animate({ scrollTop: top }, 1500);
+	});
+
+	$('.menu-toggle').on('click', function () {
+		$('.menu-toggle').toggleClass('menu-toggle--active');
+		$('.header__list-link').toggleClass('header__list-link--active');
+	});
+
 	$('.specialties__slider').slick({
 		dots: true,
 		arrows: false,
